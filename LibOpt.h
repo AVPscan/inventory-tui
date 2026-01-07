f14f29113bf52a9430b5b0c2c3f41299cd3e4581
AVPscan <avp70ru@mail.ru>
2026-01-07 16:37:49 +0700

    Обновил README и добавил авторство в исходники

--- LibOpt.h
+++ LibOpt.h
@@ -1,17 +1,13 @@
-/*
- * Copyright 2026 [Алексей Васильевич Поздняков AVPscan]
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     www.apache.org
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/* 
+ * Автор: Поздняков Алексей Васильевич
+ * Email: avp70ru@mail.ru
+ * GitHub: @AVPscan
+ * 
+ * Copyright (c) 2026 Алексей Поздняков
+ * 
+ * Лицензия: 
+ * Этот проект распространяется под условиями лицензии Apache-2.0.
+ * Полный текст лицензии можно найти в файле LICENSE в корневом каталоге.
  */
 
 #ifndef LIBOPT_H
